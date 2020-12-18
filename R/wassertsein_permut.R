90ec22f2ea44533fc2e33626ac8dcb6de0037ff9
Hector <hector.rouxdebezieux@berkeley.edu>
2020-12-18 17:48:09 +0100

    added wassertsein permutation tests

---
+++ R/wassertsein_permut.R
@@ -0,0 +1,64 @@
+#' Permutation test based on wasserstein distance
+#'
+#' @description Permutation test based on Wasserstein distance
+#'
+#' @param x Samples from the first distribution
+#' @param y Samples from the second distribution. Only used if x is a vector.
+#' @param iterations How many iterations to do to simulate the null distribution.
+#' Default to 10^4.
+#' @param fast If true, uses the \link[transport]{ubwasserstein}
+#' approximate function. Default to true if there are more than 1,000 samples
+#' total.
+#' @param S Number of samples to use in approximate mode. Must be set if `fast=TRUE`.
+#' See \link[transport]{ubwasserstein}.
+#' @param ... Other parameters passed to \link[transport]{wasserstein} or
+#' \link[transport]{wasserstein1d}
+#' @examples
+#'  x <- matrix(c(runif(100, 0, 1),
+#'                runif(100, -1, 1)),
+#'              ncol = 2)
+#'  y <- matrix(c(runif(100, 0, 3),
+#'                runif(100, -1, 1)),
+#'              ncol = 2)
+#'  wasserstein_permut(x, y)
+#' @md
+#' @return
+#' A list containing the following components:
+#' \itemize{
+#'   \item *statistic* the bwasserstein distance between x and y.
+#'   \item *p.value* the p-value of the permutation test.
+#' }
+#' @export
+#' @importFrom dplyr if_else
+#' @importFrom pbapply pblapply
+#' @importFrom transport wasserstein wasserstein1d pp subwasserstein
+#' @export
+wasserstein_permut <- function(x, y, iterations = 10^4,
+                               fast = nrow(x) + nrow(y) > 10^3,
+                               S = NULL, ...) {
+  if (ncol(x) != ncol(y)) stop("X and Y must have the same dimension")
+  d <- ncol(x)
+  nx <- nrow(x)
+  ny <- nrow(y)
+  X <- rbind(x, y)
+  if (d == 1) {
+    dist_func <- transport::wasserstein1d
+  } else {
+    dist_func <- transport::wasserstein
+  }
+  if (fast) {
+    dist_func <- transport::subwasserstein
+  }
+  if (fast & !(is.numeric(S) && S > 0)) {
+    stop("If fast mode is chosen, the S argument must be specified.")
+  }
+  og <- dist_func(transport::pp(x), transport::pp(y), S = S, ...)
+  null <- pbapply::pblapply(rep(0, iterations), function(rep) {
+    permut <- sample(nx + ny)
+    new_x <- X[permut[seq_len(nx)], ]
+    new_y <- X[permut[seq(nx + 1, nx + ny)], ]
+    return(dist_func(transport::pp(new_x), transport::pp(new_y), S = S, ...))
+  }) %>% unlist()
+  pval <- max(1 / iterations, mean(og <= null))
+  return(list("statistic" = og, "p.value" = pval))
+}
---
+++ tests/testthat/test_wasserstein.R
@@ -0,0 +1,18 @@
+library(testthat)
+
+test_that("the wasserstein permutation test works with all inputs",{
+  set.seed(08)
+  x <- matrix(c(stats::runif(20, 0, 1),
+                stats::runif(20, -1, 1)),
+              ncol = 2)
+  y <- matrix(c(stats::runif(20, 0, 1),
+                stats::runif(20, -1, 1)),
+              ncol = 2)
+  test <- wasserstein_permut(x, y)
+  expect_is(test, "list")
+  expect_true(test$statistic > 0)
+  test_fast <- wasserstein_permut(x, y, fast = TRUE, S = 10)
+  expect_is(test_fast, "list")
+  expect_true(test_fast$statistic > 0)
+  expect_error(wasserstein_permut(x, y, fast = TRUE))
+})
